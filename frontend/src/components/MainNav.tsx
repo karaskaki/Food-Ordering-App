4a14ca4acc55c0f1087d34d27872ef95bfb98bd3
Karaskaki <nik.ranchi7@gmail.com>
2024-03-04 00:13:05 +0530

    Added Authenticated user links

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,9 +1,24 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
 
 export default function MainNav() {
+
+  const {loginWithRedirect, isAuthenticated} = useAuth0()
+
   return (
-    <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white">
+    <span>{isAuthenticated ? (<UsernameMenu /> )
+    :
+    (
+      <Button 
+        variant="ghost" 
+        className=" text-xl font-bold hover:text-orange-500 hover:bg-white"
+        onClick={ async () => await loginWithRedirect() }
+      >
         Log In
     </Button>
+    )}
+    </span>
+    
   )
 }
