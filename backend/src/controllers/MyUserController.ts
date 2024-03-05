308889bc1badd18f7b1af42ed62ef8e5311e8f37
Karaskaki <nik.ranchi7@gmail.com>
2024-03-05 23:58:44 +0530

    Created Access Token

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -2,36 +2,21 @@ import { Request, Response } from "express";
 import User from "../models/user";
 
 const createCurrentUser = async (req: Request, res: Response) => {
-  // 1: Check if user exist
-  // 2: Create the user if it doesn't exist
-  // 3: return the user object to the calling client
-
   try {
     const { auth0Id } = req.body;
-
-    if (!auth0Id) {
-      return res.status(403).send({
-        success: false,
-        message: "Auth0Id not found",
-      });
-    }
-
     const existingUser = await User.findOne({ auth0Id });
 
     if (existingUser) {
-      res.status(200).send();
+      return res.status(200).send();
     }
 
     const newUser = new User(req.body);
-
     await newUser.save();
 
     res.status(201).json(newUser.toObject());
   } catch (error) {
     console.log(error);
-    res.status(500).json({
-      message: "Error in creating User",
-    });
+    res.status(500).json({ message: "Error creating user" });
   }
 };
 
--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layouts from "./layouts/layouts";
 import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const AppRoutes = () => {
         }
       />
 
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
