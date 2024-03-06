ffa7a080f22bb38578fc6b99c4a309a89cb68587
Karaskaki <nik.ranchi7@gmail.com>
2024-03-07 01:38:50 +0530

    updated the validation

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -1,4 +1,17 @@
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+
+const handleValidationErrors = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
 export const validateMyUserRequest = [
   body("name").isString().notEmpty().withMessage("Name must be a string"),
